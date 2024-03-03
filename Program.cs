0e21f06fa87c65d1d9408785f37b0b6ab4c70c99
kropli <dimakirimail@gmail.com>
2024-03-03 13:30:08 +0200

    Added file reading
    - added logic for showing file content by passed path in args
    - created test markdown file for checking base program logic in future

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Lab1
+﻿using System.Text.RegularExpressions;
+
+namespace Lab1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0) throw new Exception("any args was passed");
+
+            string readingFilePath = args[0];
+            if (!File.Exists(readingFilePath)) throw new Exception("incorrect reading file path was passed");
+
+            string fileContent = File.ReadAllText(readingFilePath);
+            Console.WriteLine(fileContent);
         }
     }
 }
