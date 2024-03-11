ad7a951ca991f2aee4e432b413273a64801d1379
kropli <dimakirimail@gmail.com>
2024-03-11 23:13:44 +0200

    Implement unit test
    - created MarkdownConverterTests class with unit tests methods
    - created dotnet.yml file to specify unit tests in github actions
    - added xunit, Microsoft.NET.Test.Sdk and xunit.runner.visualstudio packages for running unit tests
    - created InvalidMarkdownException class for better exception checking methods

---
+++ InvalidMarkdownException.cs
@@ -0,0 +1,11 @@
+﻿namespace MarkdownConverter
+{
+    public class InvalidMarkdownException : Exception
+    {
+        public InvalidMarkdownException() { }
+
+        public InvalidMarkdownException(string message) : base(message) { }
+
+        public InvalidMarkdownException(string message, Exception inner) : base(message, inner) { }
+    }
+}
